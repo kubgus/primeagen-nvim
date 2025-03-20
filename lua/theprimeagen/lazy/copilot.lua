feeda4494a47d60fdda9ae98a1281d0eb52e1a4f
kubgus <business@gustafik.com>
2025-03-21 00:12:57 +0100

    Enable copilot

--- lua/theprimeagen/lazy/copilot.lua
+++ lua/theprimeagen/lazy/copilot.lua
@@ -7,16 +7,16 @@ return {
             require("copilot").setup({
                 suggestion = {
                     enabled = true,
-                    auto_trigger = false,
+                    auto_trigger = true,
                     hide_during_completion = false,
                     debounce = 25,
                     keymap = {
                         accept = false,
                         accept_word = false,
-                        accept_line = "<Tab>",
+                        accept_line = "<S-Tab>",
                         next = false,
                         prev = false,
-                        dismiss = false,
+                        dismiss = "<C-c>",
                     },
                 },
             })
