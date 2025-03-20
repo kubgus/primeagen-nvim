6e777e02987ece6fd3bcbacc3e58290bad053aae
kubgus <business@gustafik.com>
2025-03-21 00:12:42 +0100

    Switch theme to nightfly

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -49,11 +49,12 @@ autocmd({"BufWritePre"}, {
 autocmd('BufEnter', {
     group = ThePrimeagenGroup,
     callback = function()
-        if vim.bo.filetype == "zig" then
-            vim.cmd.colorscheme("tokyonight-night")
-        else
-            vim.cmd.colorscheme("rose-pine-moon")
-        end
+        --if vim.bo.filetype == "zig" then
+        --    vim.cmd.colorscheme("tokyonight-night")
+        --else
+        --    vim.cmd.colorscheme("rose-pine-moon")
+        --end
+        vim.cmd.colorscheme("nightfly")
     end
 })
 
--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -9,6 +9,13 @@ end
 return {
 
     {
+        "bluz71/vim-nightfly-colors",
+        lazy = false,
+        config = function ()
+            ColorMyPencils()
+        end
+    },
+    {
         "erikbackman/brightburn.vim",
     },
 
@@ -57,7 +64,7 @@ return {
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
+                transparent = false, -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
