8db358d57bebab2db1813af10e0bcfd884e36cc3
kubgus <business@gustafik.com>
2025-03-28 15:53:40 +0100

    Add nvim-tree

---
+++ lua/theprimeagen/lazy/nvim-tree.lua
@@ -0,0 +1,41 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function ()
+        require("nvim-tree").setup({
+            hijack_cursor = true,
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 48,
+                float = {
+                    enable = true,
+                    open_win_config = {
+                        row = 0,
+                        col = 999,
+                        width = 48,
+                        height = 38,
+                    },
+                },
+            },
+            update_focused_file = {
+                enable = true,
+            },
+            filters = {
+                dotfiles = true,
+            },
+            renderer = {
+                group_empty = true,
+                icons = {
+                    git_placement = "after",
+                    glyphs = {
+                        folder = {
+                            arrow_closed = " ",
+                            arrow_open = " ",
+                        },
+                    },
+                },
+            },
+        })
+    end
+}
